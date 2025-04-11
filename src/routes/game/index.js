a1a4bc1bc3ea41ed541bda880915cbffe160fb55
Justino Soares <justinocsoares123@gmail.com>
2025-04-11 16:50:31 +0100

    mais

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 const { getQuestion, getResponse, alert } = require("../../controllers/game/index");
 
-
 router.get("/game/get_question",  getQuestion);
 router.get("/game/get_response",  getResponse);
 router.get("/alert/",  alert);
-
 
 module.exports = router;
