f7591c672e771106a5f1dce427d7c835e331dbbe
Justino Soares <justinocsoares123@gmail.com>
2025-04-13 08:38:37 +0100

    get notification

--- src/routes/notification/index.js
+++ src/routes/notification/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { climate } = require("../../controllers/notification/climate");
+const { userAuthorization } = require("../../middleware/authorization");
+
+const { climate, getNotification } = require("../../controllers/notification/climate");
 
 router.get("/notification/climate",  climate);
-
+router.get("/notification", userAuthorization, getNotification);
 
 module.exports = router;
