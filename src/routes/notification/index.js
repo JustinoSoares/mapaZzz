7379ef34826d4320e6ee163fa1d2486a68a9f88d
Justino Soares <justinocsoares123@gmail.com>
2025-04-13 19:25:51 +0100

    ajustes

--- src/routes/notification/index.js
+++ src/routes/notification/index.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const { userAuthorization } = require("../../middleware/authorization");
 
-const { climate, getNotification } = require("../../controllers/notification/climate");
+const { climate, getNotification, approach_danger_zone, game } = require("../../controllers/notification/climate");
 
 router.get("/notification/climate",  climate);
+
+router.get("/notification/approach_danger_zone", userAuthorization, approach_danger_zone);
+
+router.get("/notification/game", userAuthorization ,game);
+
 router.get("/notification/", userAuthorization, getNotification);
 
 module.exports = router;
