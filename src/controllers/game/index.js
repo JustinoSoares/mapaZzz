7decda8af3088f697fb52b39b5da34bbae39edc9
Justino Soares <justinocsoares123@gmail.com>
2025-05-20 08:27:14 +0100

    add points

--- src/controllers/game/index.js
+++ src/controllers/game/index.js
@@ -1,75 +1,85 @@
-const db = require('../../../models/index');
+const db = require("../../../models/index");
 const notification = db.notification;
+const User = db.User;
 exports.getQuestion = async (req, res) => {
-    try {
-        const Question = require("../../general/trainingAi/question_to_game");
-        // Call the function to get the question
-        const question = await Question.run();
-        // Send the result back to the client
-        res.json(question);
-    } catch (error) {
-        console.error("Error processing question:", error);
-        res.status(500).json({
-            error: "An error occurred while processing the question.",
-            message : error.message,
-        });
-    }
-}
+  try {
+    const Question = require("../../general/trainingAi/question_to_game");
+    // Call the function to get the question
+    const question = await Question.run();
+    // Send the result back to the client
+    res.json(question);
+  } catch (error) {
+    console.error("Error processing question:", error);
+    res.status(500).json({
+      error: "An error occurred while processing the question.",
+      message: error.message,
+    });
+  }
+};
 
 exports.getResponse = async (req, res) => {
     try {
-        const { problem, response } = req.body;
-        if (!problem || !response) {
-            return res.status(400).json({
-                message: "O problema e a resposta são importantes."
-            });
-        }
-        const ResponseAI = require("../../general/trainingAi/response");
-        // Call the function to get the response
-        const result = await ResponseAI.run(problem, response);
-
-        // Send the result back to the client
-        res.json(result);
-    } catch (error) {
-        console.error("Error processing question:", error);
-        res.status(500).json({
-            error: "An error occurred while processing the question.",
-            message: error.message
+      const { problem, response } = req.body;
+      const userId = req.userId;
+      const user = await User.findByPk(userId);
+  
+      if (!problem || !response) {
+        return res.status(400).json({
+          message: "O problema e a resposta são importantes.",
         });
+      }
+  
+      const ResponseAI = require("../../general/trainingAi/response");
+  
+      // Chama a função para verificar a resposta
+      const result = await ResponseAI.run(problem, response);
+  
+      // Se a resposta estiver correta, incrementa a pontuação
+      if (result.is_right) {
+        user.points = (user.points || 0) + 1; // garante que score começa em 0 se for null
+        await user.save(); // salva a atualização no banco de dados
+      }
+  
+      // Retorna o resultado para o cliente
+      res.json(result);
+    } catch (error) {
+      console.error("Error processing question:", error);
+      res.status(500).json({
+        error: "An error occurred while processing the question.",
+        message: error.message,
+      });
     }
-}
-
+  };
+  
 exports.alert = async (req, res) => {
-    const userId = req.userId;
-    try {
-        const alert = require("../../general/trainingAi/alert");
-        const result = await alert.run();
-        const notificationCreate = await notification.create({
-            lat: 0.0,
-            lon: 0.0,
-            typeNotification: 'educação',
-            title: 'Aviso Importante',
-            describe: result.recommendation,
-            userId: userId,
-            users: [],
-        });
-        // Emitir notificação via Socket.IO
-        const io = req.app.get('socketio');
-        io.emit('notification', {
-            data: notificationCreate,
-        });
-        return res.status(200).json({
-            message: 'Notificação criada com sucesso.',
-            notificationCreate,
-        });
-        // res.json(result);
-    } catch (error) {
-        console.error("Error processing question:", error);
-        res.status(500).json({
-            error: "An error occurred while processing the question.",
-            message: error.message
-        });
-    }
-}
-
-
+  const userId = req.userId;
+  try {
+    const alert = require("../../general/trainingAi/alert");
+    const result = await alert.run();
+    const notificationCreate = await notification.create({
+      lat: 0.0,
+      lon: 0.0,
+      typeNotification: "educação",
+      title: "Aviso Importante",
+      describe: result.recommendation,
+      userId: userId,
+      users: [],
+    });
+    // Emitir notificação via Socket.IO
+    const io = req.app.get("socketio");
+    io.emit("notification", {
+      data: notificationCreate,
+    });
+    return res.status(200).json({
+      message: "Notificação criada com sucesso.",
+      notificationCreate,
+    });
+    // res.json(result);
+  } catch (error) {
+    console.error("Error processing question:", error);
+    res.status(500).json({
+      error: "An error occurred while processing the question.",
+      message: error.message,
+    });
+  }
+};
--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { getQuestion, getResponse, alert } = require("../../controllers/game/index");
 const {userAuthorization} = require("../../middleware/authorization");
 
-router.get("/game/get_question",  getQuestion);
-router.post("/game/get_response",  getResponse);
+router.get("/game/get_question", getQuestion);
+router.post("/game/get_response", userAuthorization,  getResponse);
 router.get("/alert/", userAuthorization, alert);
 
 module.exports = router;
